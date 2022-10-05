f7fa022450c8c8b971baa00ef230a5bb8e401b29
Luiz Henrique Soares <luizhenriquesoares18@hotmail.com>
2022-10-05 15:31:05 -0300

    Adição da area timeline

---
+++ js/typingeffect.js
@@ -0,0 +1,101 @@
+/*** Plugin ***/
+
+(function ($) {
+    // writes the string
+    //
+    // @param jQuery $target
+    // @param String str
+    // @param Numeric cursor
+    // @param Numeric delay
+    // @param Function cb
+    // @return void
+    function typeString($target, str, cursor, delay, cb) {
+        $target.html(function (_, html) {
+            return html + str[cursor];
+        });
+
+        if (cursor < str.length - 1) {
+            setTimeout(function () {
+                typeString($target, str, cursor + 1, delay, cb);
+            }, delay);
+        } else {
+            cb();
+        }
+    }
+
+    // clears the string
+    //
+    // @param jQuery $target
+    // @param Numeric delay
+    // @param Function cb
+    // @return void
+    function deleteString($target, delay, cb) {
+        var length;
+
+        $target.html(function (_, html) {
+            length = html.length;
+            return html.substr(0, length - 1);
+        });
+
+        if (length > 1) {
+            setTimeout(function () {
+                deleteString($target, delay, cb);
+            }, delay);
+        } else {
+            cb();
+        }
+    }
+
+    // jQuery hook
+    $.fn.extend({
+        teletype: function (opts) {
+            var settings = $.extend({}, $.teletype.defaults, opts);
+
+            return $(this).each(function () {
+                (function loop($tar, idx) {
+                    // type
+                    typeString($tar, settings.text[idx], 0, settings.delay, function () {
+                        // delete
+                        setTimeout(function () {
+                            deleteString($tar, settings.delay, function () {
+                                loop($tar, (idx + 1) % settings.text.length);
+                            });
+                        }, settings.pause);
+                    });
+
+                }($(this), 0));
+            });
+        }
+    });
+
+    // plugin defaults  
+    $.extend({
+        teletype: {
+            defaults: {
+                delay: 100,
+                pause: 5000,
+                text: []
+            }
+        }
+    });
+}(jQuery));
+
+
+/*** init ***/
+
+$('#target').teletype({
+    text: [
+        ' Oi, eu sou o Luiz e sou Estudante de Engenharia de Software.',
+        ' Eu sou um jovem padawan no Desenvolvedor WEB. ',
+        ' Apaixonado no universo Star Wars e Jedi nas horas vagas.',
+        ' Eu sou Fã de carteirinha da banda The Lumineers.',
+        ' E por fim, mas não menos importante, sou um amante de café.'
+
+    ]
+});
+
+$('#cursor').teletype({
+    text: ['_'],
+    delay: 0,
+    pause: 500
+});
