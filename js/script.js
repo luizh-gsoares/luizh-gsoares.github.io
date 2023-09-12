cec19a0a44391986db07d3bdad4b57a3c046b813
Luiz Henrique Soares <luizhenriquesoares18@hotmail.com>
2023-09-12 16:47:58 -0300

    Fixing Repo Cards and Javascript

--- js/script.js
+++ js/script.js
@@ -1,47 +1,57 @@
 // -------------------------------------------
-//   Author: Luiz Soares
+//   Autor: Luiz Soares
 //   Copyright (c) 2022 Luiz Soares
 // -------------------------------------------
-const user = "luizh-gsoares";
+const usuario = "luizh-gsoares";
 
 function buscarRepositorios() {
-    var requestURL = 'https://api.github.com/users/' + user + '/repos';
+    var requestURL = 'https://api.github.com/users/' + usuario + '/repos';
     var request = $.get(requestURL, function () {
     })
         .done(function () {
             request = request.responseJSON;
             if (!Array.isArray(request) || !request.length) {
-                $("#repo-container").append("<div class='alert alert-danger'>Houve um erro ao listar os repositórios do GitHub, por favor, acesse manualmente em https://github.com/luizh-gsoares?tab=repositories.</div>");
+                $("#repo-container").append("<div class='alert alert-danger'> Houve um erro ao listar os repositórios do GitHub.</div>");
             }
             else {
                 for (i = 0; i < request.length; i++) {
                     // Variáveis para armazenar informações do repositório, caso queira alguma que não esteja aqui
                     // Acessar documentação do GitHub API em https://docs.github.com/en/rest?apiVersion=2022-11-28
-                    var username = request[i].owner.login;
                     var repo_url = request[i].html_url;
                     var repo_name = request[i].name;
                     var repo_description = request[i].description;
+                    var repo_created_at = new Date(request[i].created_at).toLocaleDateString();
                     var repo_language = request[i].language;
-                    var repo_stars = request[i].stargazers_count;
-                    var repo_forks = request[i].forks;
+                    var repo_topics = request[i].topics;
+
+                    // Verifica se o repositório contém um tópico 'esconder', se sim, não exibe o repositório.
+                    if (repo_topics == 'esconder') {
+                        continue;
+                    }
 
                     // Verifica se o repositório não contém descrição ou linguagem.
                     if (repo_description == null) {
-                        repo_description = "<i>Sem descrição.</i>";
+                        repo_description = "<p>Sem descrição.</p>";
                     }
+                    
                     if (repo_language == null) {
                         repo_language = "-";
                     }
 
+
                     // Colocar o repositório na página como div
                     $("#repo-container").append(
-                        "<div class='card' href='" + repo_url + "'>"
+                        "<div class='col'>"
+                        + "<div class='card h-100'>"
                         + "<div class='card-body'>"
-                        + "<h5 class='card-title'><a href='" + repo_url + "' target='_blank'>" + repo_name + "</a></h5>"
+                        + "<h5 class='card-title'><i class='fa-solid fa-code-branch'></i> " + repo_name + "</h5>"
+                        + "<h6 class='card-subtitle text-muted'>"+ repo_topics +"</h6>"
                         + "<p class='card-text'>" + repo_description + "</p>"
-                        + "<div class='row'>"
-                        + "<div class='col-6'>"
-                        + "<p class='card-text'><small class='text-muted'>Language: " + repo_language + "</small></p>"
+                        + "</div>"
+                        + "<div class= 'card-footer'>"
+                        + "<p class='text-white'><a href='"+ repo_url +"' class='stretched-link'></a></p>"
+                        + "<p class='card-text'>Linguagem : " + repo_language + "</p>"
+                        + "<small class='text-muted'> Criado em : " + repo_created_at + "</small>"
                         + "</div>"
                         + "</div>"
                         + "</div>"
