05f69b96082a25e26846dd8b16ad5a8732d91551
Luiz Henrique Soares <luizhenriquesoares18@hotmail.com>
2024-01-03 17:56:14 -0300

    Updating projects section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // -------------------------------------------
 //   Autor: Luiz Soares
-//   Copyright (c) 2023 Luiz Soares
+//   Copyright (c) 2024 Luiz Soares
 // -------------------------------------------
 
 
@@ -17,7 +17,6 @@ function requestRepositorios(usuario) {
 function criarRepositorioCard(repo) {
     var repo_topics = repo.topics || [];
 
-    // Se o repositório tiver o tópico "esconder", ele não será exibido.
     if (repo_topics.includes("esconder")) {
         return null;
     }
@@ -27,31 +26,84 @@ function criarRepositorioCard(repo) {
     var repo_stars = repo.stargazers_count || 0;
     var repo_forks = repo.forks_count || 0;
     var repo_created_at = new Date(repo.created_at).toLocaleDateString();
-    var repo_homepage = repo.homepage ? "🌎: " + repo.homepage : "";
 
-    // Cria uma string com todos os tópicos do repositório.
     var topicsHTML = repo_topics.map(topic => `<span class='badge rounded-pill bg-dark'>${topic}</span>`).join(' ');
 
-    return `
-        <div class='col' id='${repo.id}'>
-            <div class='card h-100'>
-                <div class='card-body'>
-                    <h5 class='card-title'><i class='fa-solid fa-book fa-lg'></i> ${repo.name}</h5>
+    var repoCardHTML = `
+    <div class='col' id='${repo.id}'>
+        <div class='card h-100'>
+            <div class='card-body'>
+                <h5 class='card-title text-center'><i class='fa-solid fa-book fa-lg'></i> ${repo.name}</h5>
+                <div class='text-center' style='height : 48px;'>
                     ${topicsHTML}
-                    <p class='card-text'><a href='${repo.html_url}' class='text-decoration-none'>${repo_description}</a></p>
-                    <p><a href='${repo.homepage}'>${repo_homepage}</a></p>
                 </div>
-                <div class= 'card-footer'>
-                    <small class='text-muted'>Linguagem : ${repo_language}</small>
-                    <div>
-                        <small class='text-muted'> Criado em : ${repo_created_at}</small>
-                        <small class='text-muted'><i class='fa-regular fa-star'></i> ${repo_stars}</small>
-                        <small class='text-muted'><i class='fa-solid fa-code-fork'></i> ${repo_forks}</small>
-                    </div>
-                </div>
+                <p class='py-2 card-text'>${repo_description}</p>   
+            </div>
+
+            <div class='text-center py-2'>
+                <button class='btn btn-link' data-bs-toggle='modal' data-bs-target='#repoModal' onclick='exibirDetalhesRepositorio(${JSON.stringify(repo)})'>Ver detalhes</a>
+            </div>
+
+            <div class='card-footer'>
+                <small class='text-muted'> Criado em : ${repo_created_at}</small>
+                <small class='text-muted'><i class='fa-regular fa-star'></i> ${repo_stars}</small>
+                <small class='text-muted'><i class='fa-solid fa-code-fork'></i> ${repo_forks}</small>
+                <p class='text-muted'>Linguagem : ${repo_language}</p>
             </div>
         </div>
+    </div>
     `;
+
+    return repoCardHTML;
+}
+
+// Exibe os detalhes do repositório recebido no modal.
+function exibirDetalhesRepositorio(repo) {
+    var repoModalBody = document.getElementById('repoModalBody');
+    var repo_description = repo.description || "<p>Sem descrição.</p>";
+    var repo_language = repo.language || "--";
+    var repo_stars = repo.stargazers_count || 0;
+    var repo_forks = repo.forks_count || 0;
+    var repo_created_at = new Date(repo.created_at).toLocaleDateString();
+    var topicsHTML = repo.topics.map(topic => `<span class='badge rounded-pill bg-dark'>${topic}</span>`).join(' ') || "<small>Sem tópicos</small>";
+    var repo_homepage = repo.homepage ? "🌎: " + repo.homepage : "";
+
+    var repoDetailsHTML = 
+    `
+        <h4 class='card-title text-center'><a href='${repo.html_url}' class='text-decoration-none'><i class='fa-solid fa-book fa-lg'></i> ${repo.name}</a></h4>
+        <hr>
+        <div class='text-center' style='height : 24px;'>
+            ${topicsHTML}
+        </div>
+        <p class='py-2'> <a href='${repo.html_url}' class='text-decoration-none'><strong> Descrição :</strong> ${repo_description}</a></p>
+        <p><a href='${repo.homepage}'>${repo_homepage}</a></p>
+        <p><strong> Criado em:</strong> ${repo_created_at} </p>
+        <p><strong> Linguagem:</strong> ${repo_language} <i class='fa-regular fa-star'></i> ${repo_stars} <i class='fa-solid fa-code-fork'></i> ${repo_forks}</p> 
+        <div id="readmeContent"></div>
+    `;
+
+    // Exibe as informações do repositório no modal
+    repoModalBody.innerHTML = repoDetailsHTML;
+
+    // Faz uma solicitação para obter o conteúdo do README.md do repositório
+    $.get(`https://api.github.com/repos/${repo.full_name}/readme`)
+        .done(function (readme) {
+            // Decodifica o conteúdo do README.md
+            var readmeContent = b64DecodeUnicode(readme.content);
+
+            // Converte o conteúdo do README.md de Markdown para HTML
+            var converter = new showdown.Converter();
+            var html = converter.makeHtml(readmeContent);
+
+            // Exibe o conteúdo do README.md no modal
+            var readmeContentDiv = document.getElementById('readmeContent');
+            readmeContentDiv.innerHTML = "<h5 class='text-center'>README.md</h5><hr>" + html;
+        })
+        .fail(function () {
+            // Caso ocorra algum erro, exibe uma mensagem de erro
+            var readmeContentDiv = document.getElementById('readmeContent');
+            readmeContentDiv.innerHTML = "<div class='alert alert-danger'> README não encontrado.</div>";
+        });
 }
 
 // Exibe os repositórios recebidos na tela.
@@ -79,6 +131,13 @@ function buscarRepositorios() {
         .fail(function () {
             $("#error-message").append("<div class='alert alert-danger'> Houve um erro ao listar os repositórios do GitHub.</div>");
         });
+}
+
+// A função para decodificar a string codificada em base64 com caracteres Unicode
+function b64DecodeUnicode(str) {
+    return decodeURIComponent(atob(str).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 }
 
 // Chame a função buscarRepositorios() para iniciar o processo.
